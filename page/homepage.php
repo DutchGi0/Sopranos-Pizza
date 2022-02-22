f235ad7b2e1d9b691eab350c5d9ee5799e58dea7
Gio <113716@mborijnland.nl>
2022-02-22 21:01:21 +0100

    wireframes gedaan
    Homepage zo goed als af

--- page/homepage.php
+++ page/homepage.php
@@ -5,25 +5,55 @@
     content="text/html"; charset="utf-8">
     <title>Pizza sopranos</title>
     <link rel="stylesheet" href="bootstrap-5.0.2-dist/bootstrap-5.0.2-dist/css/bootstrap.css"/>
-    <link rel="stylesheet" href="css/styles.css"/>
+    <link rel="stylesheet" href="components\css\styles.css"/>
 </head>
 <body>
     <div class="title">
         <h1>Pizza sopranos</h1>
     </div>
     <hr class="secondary">
-    <div class="container-fluid">
-        <div class="main">
-            <!-- <p>Moet background hebben</p> -->
+    <main>
+        <div class="container-fluid main">
+            <div>
+                <div class="row justify-content-end">
+                    <div class="main-text col">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi rem cumque labore repudiandae veniam aut cum laboriosam possimus atque! 
+                    </div>
+                    <div class="col-sm-2 reviews">
+                        <div class="reviews-title">
+                            Reviews
+                        </div> 
+                        <div class="reviews-text">
+                            <!-- Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur aut ratione voluptates quo, quaerat tempore soluta similique maiores expedita adipisci alias architecto, ab magnam ducimus illum eaque quod error cum. Id nemo recusandae omnis necessitatibus nam exercitationem nihil quidem obcaecati blanditiis. Perspiciatis velit repudiandae illo beatae ipsam id, vitae quod excepturi tempora quaerat unde distinctio dolorem deserunt? Exercitationem, eaque expedita! -->
+                        </div>
+                    </div>
+                    <div class="col-sm-2 acties">
+                        <div class="acties-title">
+                            Acties
+                        </div> 
+                        <div class="acties-text">
+                            <!-- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, nostrum deserunt hic expedita corrupti incidunt explicabo at dicta placeat corporis illum dolore natus esse quod, tempore odio dolor ipsa aut atque! Sapiente illum officiis, modi mollitia deserunt corrupti tempora, eos molestiae neque nulla labore cupiditate. Qui minus sunt ut velit iste placeat laborum numquam quia, earum dolore explicabo. Neque, mollitia? -->
+                            <a href="http://www.google.com/">
+                                <input type="button" value="Bestel online" class="btn-acties"/>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row justify-content-start">
+                        <div class="col-sm-7 menu">
+                            <div class="menu-title">Menu</div>
+                            <div class="menu-items">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae numquam veniam reprehenderit doloribus nobis facilis, possimus modi temporibus enim architecto eos voluptatum accusamus minima, sunt distinctio ipsum consectetur libero amet!
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    
+                    
+                </div>
+            </div>    
         </div>
-
-        <div class="row">
-            <div class="col-sm-8 menu">Menu</div>
-        </div>
-        <div class="row">
-            <div class="col-sm-2 reviews">Reviews</div>
-        </div>
-    </div>
+    </main>
 <script src="https://unpkg.com/@popperjs/core@2.4.0/dist/umd/popper.min.js"></script>
 <script src="bootstrap-5.0.2-dist/bootstrap-5.0.2-dist/js/bootstrap.js"></script>
 </body>
